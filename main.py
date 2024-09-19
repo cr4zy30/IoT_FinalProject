afe01bc7ce63e1c3d5801cab0d53653b66bc070a
cr4zy30 <vorden2005@gmail.com>
2024-09-19 09:45:16 -0400

    Switching images on/off

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 from flask import Flask, render_template, jsonify
+import RPi.GPIO as GPIO
 
 app = Flask(__name__)
 
+LED=25
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(LED, GPIO.OUT)
+GPIO.output(LED, 0)
 led_state=False
 
 @app.route("/", methods=["GET"])
 def home():
-    return render_template('index.html')
+    return render_template('dashboard.html')
 
 @app.route("/switch_led", methods=["GET"])
 def get_led_state():
     global led_state
     led_state = not led_state
+    GPIO.output(LED, led_state)
     data = {
         "led_state": led_state
     }
